b2d24d11f0529b1aceb19f34b41a8b4b321e9fc3
giu7d <giuseppe7d@gmail.com>
2025-09-02 17:00:59 -0300

    fix: add cedilla to gtk and qt

--- hosts/__default__/default.nix
+++ hosts/__default__/default.nix
@@ -48,8 +48,14 @@
     LC_PAPER = "pt_BR.UTF-8";
     LC_TELEPHONE = "pt_BR.UTF-8";
     LC_TIME = "pt_BR.UTF-8";
-    LC_CTYPE = lib.mkDefault "pt_BR.UTF-8"; # Fix ç in us-intl.
+    LC_CTYPE = lib.mkDefault "pt_BR.UTF-8"; # Add cedilla support
   };
+  # Add cedilla support
+  environment.variables = {
+    GTK_IM_MODULE = "cedilla";
+    QT_IM_MODULE = "cedilla";
+  };
+
   #
   # Base Applications
   #
