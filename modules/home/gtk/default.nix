de46921bd9e48df4192ee043b3ee7ee673ed02e2
giu7d <giuseppe7d@gmail.com>
2025-09-03 18:49:28 -0300

    feat: add gsettings support

--- modules/home/gtk/default.nix
+++ modules/home/gtk/default.nix
@@ -26,20 +26,20 @@
       # Setup dash-to-dock
       "org/gnome/shell/extensions/dash-to-dock" = {
         apply-custom-theme = true;
-        background-opacity = 0.5;
+        background-opacity = "0.5";
         custom-background-color = false;
         custom-theme-shrink = true;
-        dash-max-icon-size = 32;
+        dash-max-icon-size = "32";
         disable-overview-on-startup = true;
         dock-position = "'BOTTOM'";
-        height-fraction = 0.9;
+        height-fraction = "0.9";
         hide-tooltips = false;
         intellihide-mode = "'FOCUS_APPLICATION_WINDOWS'";
         isolate-workspaces = false;
         multi-monitor = true;
-        preferred-monitor = -2;
+        preferred-monitor = "-2";
         preferred-monitor-by-connector = "'DP-3'";
-        preview-size-scale = 0.0;
+        preview-size-scale = "0.0";
         show-favorites = true;
         show-trash = true;
         show-windows-preview = true;
