279d059f53a8d520997b73cbcf93cfddff09a284
giu7d <giuseppe7d@gmail.com>
2025-09-03 12:24:21 -0300

    feat: add accent color to gdm

--- modules/gdm/default.nix
+++ modules/gdm/default.nix
@@ -5,4 +5,18 @@
   ...
 }: {
   services.xserver.displayManager.gdm.enable = lib.mkForce true;
+
+  programs.dconf.profiles.gdm = {
+    databases = [
+      {
+        lockAll = true;
+        settings = {
+          "org/gnome/desktop/interface" = {
+            accent-color = "slate";
+            color-scheme = "prefer-dark";
+          };
+        };
+      }
+    ];
+  };
 }
