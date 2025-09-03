833115f7aa181e3f26fe845ff6a0f3f3f89f7f4c
giu7d <giuseppe7d@gmail.com>
2025-09-03 18:05:00 -0300

    feat: add gnome extensions

--- modules/home/gtk/default.nix
+++ modules/home/gtk/default.nix
@@ -1,9 +1,27 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
-  imports = [
-    ./theme.nix
+  # Setup extensions
+  home.packages = with pkgs; [
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.appindicator
+    gnomeExtensions.bluetooth-battery-meter
+    gnomeExtensions.gsconnect
   ];
+
+  # Setup themes
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "WhiteSur";
+      package = pkgs.whitesur-icon-theme.override {
+        boldPanelIcons = true;
+        alternativeIcons = true;
+      };
+    };
+  };
 }
