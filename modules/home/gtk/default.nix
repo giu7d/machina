c832d7feaf5957fe267bdbf82808c40048a5b71f
giu7d <giuseppe7d@gmail.com>
2025-09-04 13:37:29 -0300

    feat: add center new window param

--- modules/home/gtk/default.nix
+++ modules/home/gtk/default.nix
@@ -10,11 +10,13 @@
   ];
 
   dconf.settings = {
-    #
     # Setup title bar buttons
-    #
     "org/gnome/desktop/wm/preferences" = {
       button-layout = ":minimize,maximize,close";
+    };
+    # Setup center new windows
+    "org/gnome/mutter" = {
+      center-new-windows = true;
     };
   };
 }
