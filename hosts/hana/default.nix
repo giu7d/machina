14f4ee9a175f4f4219bf61cac387eadc9ccd7d71
giu7d <giuseppe7d@gmail.com>
2025-09-27 13:15:34 -0300

    feat: add firewall rules to allow kde connect

--- hosts/hana/default.nix
+++ hosts/hana/default.nix
@@ -34,6 +34,20 @@
   networking.hostName = "hana";
   networking.networkmanager.enable = true;
   networking.firewall.enable = true;
+  networking.firewall.allowedTCPPortRanges = [
+    # KDE Connect
+    {
+      from = 1714;
+      to = 1764;
+    }
+  ];
+  networking.firewall.allowedUDPPortRanges = [
+    # KDE Connect
+    {
+      from = 1714;
+      to = 1764;
+    }
+  ];
 
   #
   # Desktop Environment
