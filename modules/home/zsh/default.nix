a9d87d8449d8643d0e8849d446564fc0032c9631
giu7d <giuseppe7d@gmail.com>
2025-10-03 13:34:45 -0300

    feat: improve nix-upgrade script

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -10,22 +10,6 @@
     rev = "v1.5.2";
     sha256 = "09y419rcylm5l6qy8pjj90zk4lx8b1vanbkdi7wcl03wngndwwv4";
   };
-
-  scriptNixUpgrade = pkgs.stdenv.mkDerivation {
-    pname = "nix-upgrade";
-    version = "1.0";
-
-    src = builtins.path {
-      path = ./scripts/nix-upgrade.sh;
-      name = "nix-upgrade.sh";
-    };
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src $out/bin/nix-upgrade.sh
-      chmod +x $out/bin/nix-upgrade.sh
-    '';
-  };
 in {
   programs.zsh = {
     # Main ZDOTDIR config directory
@@ -74,7 +58,7 @@ in {
 
       # Nix aliases
       nix-check = "nix flake check $FLAKE_HOME";
-      nix-upgrade = "${scriptNixUpgrade}/bin/nix-upgrade.sh";
+      nix-upgrade = "${./scripts/nix-upgrade.sh}";
       nix-rebuild = "sudo nixos-rebuild switch --flake $FLAKE_HOME#$FLAKE_HOST";
       nix-rebuild-boot = "sudo nixos-rebuild boot --flake $FLAKE_HOME#$FLAKE_HOST";
     };
