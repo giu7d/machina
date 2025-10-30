d9217f1cee42b76cf7d967f85cf10efe045dcb59
giu7d <giuseppe7d@gmail.com>
2025-10-30 13:41:06 -0300

    feat: setup hashed user password

--- users/giuseppe/default.nix
+++ users/giuseppe/default.nix
@@ -4,11 +4,14 @@
   ...
 }: let
 in {
+  sops.secrets.giuseppe-password.neededForUsers = true;
+
   nix.settings.trusted-users = ["root" "giuseppe"];
 
   users.users.giuseppe = {
     isNormalUser = true;
     description = "Giuseppe";
+    hashedPasswordFile = config.sops.secrets.giuseppe-password.path;
     shell = pkgs.zsh;
     extraGroups = [
       "networkmanager"
