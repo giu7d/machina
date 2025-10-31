95b84ea4e3b84e0b2cd6083e4a3d7db04fac765f
giu7d <giuseppe7d@gmail.com>
2025-10-30 21:15:04 -0300

    refactor: rename global age key

--- hosts/__default__/secrets.nix
+++ hosts/__default__/secrets.nix
@@ -7,7 +7,7 @@
       # automatically import host ssh keys as age keys
       sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
       # this will use an age key that is expected to already be in the FS
-      keyFile = "/var/lib/sops-nix/key.txt";
+      keyFile = "/var/lib/sops-nix/keys.txt";
       # generate a new key if the specified does not exists
       generateKey = true;
     };
