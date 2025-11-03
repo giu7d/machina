f953ed53b33d6f1b997fba06262fab0252a0ac99
giu7d <giuseppe7d@gmail.com>
2025-11-03 15:30:18 -0300

    feat: setup dev shell to startup with zsh

--- shells/dev.nix
+++ shells/dev.nix
@@ -3,4 +3,12 @@ pkgs.mkShell {
   packages = [
     pkgs.nodejs
   ];
+
+  buildInputs = [pkgs.zsh];
+  shellHook = ''
+    export SHELL=${pkgs.zsh}/bin/zsh
+    if [ -z "$ZSH_VERSION" ]; then
+      exec $SHELL
+    fi
+  '';
 }
